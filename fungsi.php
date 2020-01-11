26f6e82d84bef990d4121270d1baed6926ccc593
rokhimatuszahro <57609574+rokhimatuszahro@users.noreply.github.com>
2020-01-11 20:13:15 +0700

    unggah
    unggah file

---
+++ fungsi.php
@@ -0,0 +1,290 @@
+<?php 
+//koneksi database
+$conn = mysqli_connect("localhost","root","","tamanbotani");
+
+//fungsi untuk menampilkan data dari database
+function tampil($query){
+	global $conn;
+	$result = mysqli_query($conn,$query);
+	$rows = [];
+	while ($row  = mysqli_fetch_assoc($result)) {
+		$rows[] = $row;
+	}
+		return $rows;
+	}
+	
+//fungsi untuk menyimpan data registrasi pembeli ke dalam database
+function registrasi($data){
+	global $conn;
+ 	$nama = htmlspecialchars($data["nama"]);
+ 	$jenkel = htmlspecialchars($data["jeniskelamin"]);
+ 	$email = htmlspecialchars($data["email"]);
+ 	$password = htmlspecialchars($data["pwd"]);
+ 	$pin = htmlspecialchars($data["pin"]);
+
+ 	//query insert data
+ 	$query = "INSERT INTO users values ('','$nama','$jenkel','$email','$password','$pin','1','default.jpg')";
+ 	mysqli_query($conn,$query);
+ 	return mysqli_affected_rows($conn);
+ }
+
+//fungsi untuk menyimpan data registrasi admin ke dalam database
+function registrasiadmin($data){
+	global $conn;
+ 	$nama = htmlspecialchars($data["nama"]);
+ 	$jenkel = htmlspecialchars($data["jeniskelamin"]);
+ 	$email = htmlspecialchars($data["email"]);
+ 	$password = htmlspecialchars($data["pwd"]);
+ 	$pin = htmlspecialchars($data["pin"]);
+
+ 	//query insert data
+ 	$query = "INSERT INTO users values ('','$nama','$jenkel','$email','$password','$pin','2','default.jpg')";
+ 	mysqli_query($conn,$query);
+ 	return mysqli_affected_rows($conn);
+ }
+
+ //fungsi untuk menyimpan data pemesanan admin ke dalam database
+function pemesanan($data){
+	global $conn;
+	$id = $_SESSION['id'];
+	$tanggal = htmlspecialchars($data["tgl"]);
+	$total = htmlspecialchars($data["total"]);
+	$jumlah = htmlspecialchars($data["jumlah"]);
+ 	$nama_pemesan = htmlspecialchars($data["nama"]);
+ 	$alamat = htmlspecialchars($data["alamat"]);
+ 	$no_telp = htmlspecialchars($data["no_telp"]);
+ 	//query insert data
+ 	$query = "INSERT INTO pemesanan values ('','$nama_pemesan','$alamat','$tanggal','$jumlah','$total','$no_telp','','','','$id')";
+ 	mysqli_query($conn,$query);
+ 	return mysqli_affected_rows($conn);
+ }
+
+
+ //fungsi untuk menyimpan data pengunjung ke dalam database
+function pengunjung($data){
+	global $conn;
+ 	$tgl_pengunjung = htmlspecialchars($data["tanggal"]);
+ 	$jum_pengunjung = htmlspecialchars($data["jumlah_pengunjung"]);
+
+ 	//query insert data
+ 	$query = "INSERT INTO pengunjung values ('','$tgl_pengunjung','$jum_pengunjung')";
+ 	mysqli_query($conn,$query);
+ 	return mysqli_affected_rows($conn);
+ }
+
+//fungsi untuk menghapus data dalam database
+ function hapus($id){
+ 	global $conn;
+ 	mysqli_query($conn, "DELETE FROM pengunjung WHERE id_pengunjung = $id");
+ 	return mysqli_affected_rows($conn);
+ }
+
+ //fungsi untuk menyimpan data harga ke dalam database
+function harga($data){
+	global $conn;
+ 	$hari = htmlspecialchars($data["hari"]);
+ 	$harga = htmlspecialchars($data["harga"]);
+
+ 	//query insert data
+ 	$query = "INSERT INTO harga values ('','$hari','$harga')";
+ 	mysqli_query($conn,$query);
+ 	return mysqli_affected_rows($conn);
+ }
+
+//fungsi untuk menghapus data dalam database
+function hapusharga($id){
+ 	global $conn;
+ 	mysqli_query($conn, "DELETE FROM harga WHERE id_harga = $id");
+ 	return mysqli_affected_rows($conn);
+ }
+
+		
+//fungsi untuk mengubah (edit) data pengunjung dalam database
+ function ubah($data){
+ 	global $conn;
+ 	$id_pengunjung = htmlspecialchars($data["id"]);
+ 	$tgl_pengunjung = htmlspecialchars($data["tanggal"]);
+ 	$jum_pengunjung = htmlspecialchars($data["jumlah_pengunjung"]);
+
+ 	//query update data
+ 	$query = "UPDATE pengunjung SET
+ 				tgl_pengunjung = '$tgl_pengunjung',
+ 				jum_pengunjung = '$jum_pengunjung'
+ 				where id_pengunjung = '$id_pengunjung'
+ 				";
+ 	$result = mysqli_query($conn,$query);
+ 	return mysqli_affected_rows($conn);
+ }
+
+//fungsi untuk mengubah data harga dalam database
+ function ubahharga($data){
+ 	global $conn;
+ 	$id_harga = htmlspecialchars($data["id"]);
+ 	$hari = htmlspecialchars($data["hari"]);
+ 	$harga = htmlspecialchars($data["harga"]);
+
+ 	//query update data
+ 	$query = "UPDATE harga SET
+ 				hari = '$hari',
+ 				harga = '$harga'
+ 				where id_harga = '$id_harga'
+ 				";
+ 	$result = mysqli_query($conn,$query);
+ 	return mysqli_affected_rows($conn);
+ }
+
+ //fungsi untuk mengubah data akun dalam database
+ function ubah_akun($data){
+	global $conn;
+	$id_user = $_SESSION['id'];
+	$nama = htmlspecialchars($data["nama"]);
+	$jeniskelamin = htmlspecialchars($data["jeniskelamin"]);
+	$email = htmlspecialchars($data["email"]);
+	$pwd = htmlspecialchars($data["pwd"]);
+	$pin = htmlspecialchars($data["pin"]);
+
+	//query update data
+	$query = "UPDATE users SET
+				nama = '$nama',
+				jenkel = '$jeniskelamin',
+				email = '$email',
+				password = '$pwd',
+				pin = '$pin'
+				where id_user = '$id_user'
+				";
+	$result = mysqli_query($conn,$query);
+	return mysqli_affected_rows($conn);
+}
+
+
+
+//fungsi untuk mengunggah bukti pembayaran  ke dalam database
+function unggah($data){
+	global $conn;
+	$iduser = $_SESSION['id'];
+ 	//upload gambar
+ 	$gambar = upload();
+ 	if (!$gambar) {
+ 		return false;
+ 	}
+ 	//query update tabel pemesanan data bukti pembayaran dimana status pembayaran 0
+ 	$query = "UPDATE pemesanan SET
+ 				bukti_pembayaran = '$gambar'
+ 				where id_user = '$iduser' AND status_pembayaran = 0
+ 				";
+ 	$result = mysqli_query($conn,$query);
+ 	return mysqli_affected_rows($conn);
+ }
+
+function upload() {
+	$namafile = $_FILES['gambar']['name'];
+	$ukuranfile = $_FILES['gambar']['size'];
+	$error = $_FILES['gambar']['error'];
+	$tmpname = $_FILES['gambar']['tmp_name'];
+	//cek apakah tidak ada gambar yang diupload
+	if ($error === 4) {
+		echo "
+			<script>
+			alert('Silahkan masukkan bukti pembayaran!');
+			</script>";
+		return false;
+	}
+
+	//cek apakah yang diupload adalah gambar
+	$ekstensigambarvalid = ['jpg', 'jpeg', 'png'];
+	$ekstensigambar = explode('.', $namafile);
+	$ekstensigambar = strtolower(end($ekstensigambar));
+	if (!in_array($ekstensigambar, $ekstensigambarvalid)) {
+		echo "
+			<script>
+			alert('Format file tidak didukung!');
+			document.location.href = 'homepembeli.php';
+			</script>";
+			exit;
+	}
+
+	//cek jika ukurannya terlalu besar
+	if ($ukuranfile > 1000000) {
+		echo "
+			<script>
+			alert('Ukuran Gambar Terlalu Besar!');
+			document.location.href = 'homepembeli.php';
+			</script>";
+			exit;
+	}
+
+	$namagambar = uniqid();
+	$namagambar	.= '.';
+	$namagambar	.= $ekstensigambar;
+	//lolos pengecekan, gambar siap diupload
+	move_uploaded_file($tmpname, 'admin/fotobuktipembayaran/'.$namagambar);
+	return $namagambar;
+}
+
+
+
+
+//fungsi untuk mengubah foto profil
+function ubahfoto($data){
+	global $conn;
+	$iduser = $_SESSION['id'];
+ 	//upload foto
+ 	$foto = uploadfoto();
+ 	if (!$foto) {
+ 		return false;
+ 	}
+ 	//query update foto profil
+ 	$query = "UPDATE users SET
+ 				foto = '$foto'
+ 				where id_user = '$iduser'
+ 				";
+ 	$result = mysqli_query($conn,$query);
+ 	return mysqli_affected_rows($conn);
+ }
+
+function uploadfoto() {
+	$namafile = $_FILES['foto']['name'];
+	$ukuranfile = $_FILES['foto']['size'];
+	$error = $_FILES['foto']['error'];
+	$tmpname = $_FILES['foto']['tmp_name'];
+	//cek apakah tidak ada foto yang diupload
+	if ($error === 4) {
+		echo "
+			<script>
+			alert('Pilih Foto Profil!');
+			</script>";
+		return false;
+	}
+
+	//cek apakah yang diupload adalah gambar
+	$ekstensifotovalid = ['jpg', 'jpeg', 'png'];
+	$ekstensifoto = explode('.', $namafile);
+	$ekstensifoto = strtolower(end($ekstensifoto));
+	if (!in_array($ekstensifoto, $ekstensifotovalid)) {
+		echo "
+			<script>
+			alert('Yang Anda Upload Bukan Foto!');
+			document.location.href = 'akun.php';
+			</script>";
+			exit;
+	}
+
+	//cek jika ukurannya terlalu besar
+	if ($ukuranfile > 1000000) {
+		echo "
+			<script>
+			alert('Ukuran foto Terlalu Besar!');
+			document.location.href = 'akun.php';
+			</script>";
+			exit;
+	}
+
+	$namafoto = uniqid();
+	$namafoto	.= '.';
+	$namafoto	.= $ekstensifoto;
+	//lolos pengecekan, foto siap diupload
+	move_uploaded_file($tmpname, 'admin/fotoprofil/'.$namafoto);
+	return $namafoto;
+}
+
+ ?>
---
+++ lupapassword1.php
@@ -0,0 +1,271 @@
+<?php 
+session_start();
+require 'fungsi.php';
+require 'PHPMailer-5.2.13/PHPMailerAutoload.php';
+
+if (isset($_SESSION['login'])) {
+	header("location:homepembeli.php");
+}
+
+
+// untuk lupa password
+if (isset($_POST['reset'])) {
+	$email = $_POST['email'];
+	$pin = $_POST['pin'];
+	$query = "SELECT * FROM users WHERE email = '$email'";
+	$result = mysqli_query($conn,$query);
+	$cek = mysqli_num_rows($result);
+	// cek apakah ada email didata base
+	if ($cek == 1) {
+		$data = mysqli_fetch_array($result);
+		// cek apakah pin sama dengan yg di database
+		if ($pin == $data['pin']) {
+			$passbaru = substr(base_convert(sha1(uniqid(mt_rand())),16,36),0,6);
+			// Update password baru
+			mysqli_query($conn,"UPDATE users SET password = $passbaru WHERE email = $email");
+
+			$mail = new PHPMailer;
+			//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+			$mail->isSMTP();                                      // Set mailer to use SMTP
+			$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+			$mail->SMTPAuth = true;                               // Enable SMTP authentication
+			$mail->Username = 'rmatuszahro@gmail.com';                 // SMTP username
+			$mail->Password = 'RAmatus99';                           // SMTP password
+			$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+			$mail->Port = 465;                                    // TCP port to connect to
+
+			// $mail->From = 'AdminDeveloper@gmail.com';
+			$mail->FromName = 'Admin Developer';
+			$mail->addAddress($email, 'User');     // Add a recipient
+			// $mail->addAddress('ellen@example.com');               // Name is optional
+			$mail->addReplyTo('rmatuszahro@gmail.comm', 'Admin');
+			$mail->addCC('rmatuszahro@gmail.com');
+			$mail->addBCC('rmatuszahro@gmail.com');
+
+			$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+			$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+			$mail->isHTML(true);                                  // Set email format to HTML
+
+			$mail->Subject = 'AdminDeveloper';
+			$mail->Body    = '<center><p>Password Berhasi Diganti</p><br>
+								<h1 class="alert alert-danger">'.$passbaru.'</h1><br>
+								<a href="http://localhost/penjualantikettamanbotaniv2/homepembeli.php">
+								Silahkan Cek Password Baru Anda</a>
+							  </center>';
+			$mail->AltBody = 'bisa This is the body in plain text for non-HTML mail clients';
+
+			if(!$mail->send()) {
+			    echo 'Message could not be sent.';
+			    echo 'Mailer Error: ' . $mail->ErrorInfo;
+			} else {
+			    echo '<h1 class="alert alert-success text-center">Password berhasil dikirim silahkan cek diemail anda</h1><br>
+			    	<center>
+			    	 <a href="http://www.gmail.com">cek email</a>
+			    	</center>';
+			}
+		}else{
+			echo "<script>
+				  	alert('PIN yang anda masukkan salah!!!');
+				  	document.location.href='lupapassword1.php';
+				  </script>";
+				  exit();	
+		}
+	}else{
+		echo "<script>
+			  	alert('Email anda tidak terdaftar');
+			  	document.location.href='lupapassword1.php';
+			  </script>";
+	}
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Lupa Password</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href='foto/2Logo-Botani-1.png' rel='shortcut icon'>
+	<!-- My CSS -->
+	<link rel="stylesheet" type="text/css" href="lupapassword1.css">
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/all.css">
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  	<script src="js/jquery-3.4.1.min.js"></script>
+  	<script src="js/popper.min.js"></script>
+  	<script src="js/bootstrap.js"></script>
+  	<script src="js/all.js"></script>
+  	<script src="js/required.js"></script>
+</head>
+<body>
+
+<!-- Header -->
+
+<section>
+	<header class="transparan">
+		<div class="container">
+			<nav class="navbar navbar-expand-lg navbar-dark mt-auto mb-2">
+			  	<a href="homepembeli.php">
+			  		<img src="foto/2Logo-Botani-1.png" height="90" width="90">
+			  	</a>
+			  	<div class="nav-item">
+				  	<h1 class="text-white text-uppercase font-weight-bolder ml-4 shadow-sm">taman botani</h1>
+			  	</div>
+
+			  		<!-- jika session login sudah login / true makan akan menjalakan isi dari if -->
+			  	<?php if (isset($_SESSION['login'])) :?>
+			  		<div class="ml-auto dropdown">
+
+			  			<!-- fungsi untuk memanggil data user yg sedang login saat ini bedasarkan session user id -->
+			    <?php
+			    	$id = $_SESSION['id'];
+					$pengguna = tampil("SELECT * FROM users WHERE id_user = $id");
+				?>
+
+						<!-- Menampilakan data array pada var $pengguna dengan looping -->
+					  <?php foreach ($pengguna as $data) { ?>
+              				<a  href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                			<span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= $data['nama'] ?></span>
+                			<img class="rounded-circle" src="admin/fotoprofil/<?= $data['foto']; ?>">
+              				</a>
+					  <?php } ?>
+
+				            <!-- Dropdown - User Information -->
+				            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+				            <a class="dropdown-item" href="akun.php">
+				                <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+				                  Pengaturan profil
+				            </a>
+				            <div class="dropdown-divider"></div>
+				                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+				                <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+				                  Keluar
+				                </a>
+				            </div>
+			  		</div>
+
+			  		<!-- jika bukan session login maka tampilkan yg ada didalam elseif -->
+			  	<?php elseif (!isset($_SESSION['login'])) :?>
+			  	<div class="ml-auto">
+                    <a href= "login1.php">
+                    <button class="btn btn-light rounded-pill " type="submit">Masuk <i class="fas fa-user"></i></button>
+                    </a>
+                 </div>
+			  	<?php endif; ?>
+
+			</nav>
+		</div>
+	<div class="transparan2">
+	</div>
+	</header>
+</section>
+
+<!-- Akhir Header -->
+
+
+<!-- Konten -->
+
+<section>
+<content>
+	<div class="container"> 
+	    <div class="konten container">
+			<div class="row">
+
+				<!-- Colom Gambar -->
+				<div class="left col-sm-12 col-md-12 col-xl-6">
+					<img src="foto/taman-botani-sukorambi.png" class="gambar1 card-img-top">
+					<img src="foto/2444221531.jpg" class="gambar2 card-img-top">
+				</div>
+
+				<!-- Colom Konten -->
+				<div class="right col-sm-12 col-md-12 col-xl-6 p-5">
+					<h1 class="text-center">Lupa Password</h1>
+
+					<!-- Form Konten -->
+					<form method="POST" action="" class="needs-validation" novalidate>
+						
+						<!-- Input PIN -->
+						<div class="form-group">
+							<label for="pin">PIN</label>
+							<input type="text" class="form-control rounded-pill" id="pin" required="" name="pin">
+							<!-- Required Bootstrap -->
+							<div class="invalid-tooltip w-50 text-center mx-auto mt-1" style="position: relative;">
+								Harap Masukkan PIN Terlebih Tahulu!
+							</div>
+							<div class="valid-tooltip w-50 text-center mx-auto mt-1" style="position: relative;">
+								PIN Sudah Dimasukkan.
+							</div>
+						</div>
+						
+						<!-- Input Email -->
+						<div class="form-group">
+							<label for="email">Email</label>
+							<input type="Email" class="form-control rounded-pill" id="email" required="" name="email">
+
+							<!-- Required Bootstrap -->
+							<div class="invalid-tooltip w-50 text-center mx-auto mt-1" style="position: relative;">
+								 Harap Masukkan Email Terlebih Tahulu!
+							</div>
+							<div class="valid-tooltip w-50 text-center mx-auto mt-1" style="position: relative;">
+								Email Sudah Dimasukkan.
+							</div>
+						</div>
+
+						<!-- Button -->
+						<div class="form-group">
+							<br><hr>
+							<div >
+								<button class="btn btn-primary rounded-pill" type="submit" name="reset">Ganti Password</button>
+							</div>
+							<br><br>
+							<div>
+								<a href="lupapassword1.php" class="float-left">Lupa Password?</a>
+								<a href="registrasipembeli.php" class="float-right">Registrasi</a>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</content>
+</section>
+
+<!-- Akhir Konten -->
+
+<!-- Footer -->
+
+<section>
+<footer>
+    <div class="container text-center text-white">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="mt-3">
+                  Kontak Kami  
+                  <i class="fab fa-whatsapp"></i>
+                  <i class="fas fa-phone-square-alt"></i>
+                  <i class="fas fa-envelope-square"></i>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="mt-3">
+                    <h4>Taman Botani</h4>
+                </div>
+ 	        </div>
+            <div class="col-md-4">
+                <div class="mt-3">
+                  Ikuti Kami
+                  <i class="fab fa-facebook-f"></i>
+                  <i class="fab fa-instagram"></i>
+                </div>
+            </div>			
+        </div>
+    </div>
+</footer>
+</section> 
+
+<!-- Akhir Footer -->
+          
+</body>
+</html>
