059b3264498374b7a878662d90254a090b0f7c26
musbihatulc <57608234+musbihatulc@users.noreply.github.com>
2020-01-11 21:58:13 +0700

    print
    tampilan print

--- print.php
+++ print.php
@@ -43,54 +43,81 @@ if (isset($_POST['printdetail'])) {
   	<script src="js/all.js"></script>
 </head>
 <body>
-	<?php foreach ($printpemesanan as $data): ?>
-	<div class="container">
-	<h1><center>Bukti Pemesanan Tiket Taman Botani</center><h1>
-	
-	<div class="content">
-	<form method="POST" action="">
-	<table>
-		<tr>
-			<td><label for="nama_pemesan">Nama Pemesan</label></td>
-			<td>:</td>
-			<td><Input type="text" name="nama_pemesan" value="<?= $data["nama_pemesan"]; ?>"> </td>
-		</tr>	
 
-		<tr>
-			<td><label for="alamat">Alamat</label></td>
-			<td>:</td>
-			<td><input type="text" name="alamat" value="<?= $data['alamat']; ?>"> </td>
-		</tr>
+<!-- Header -->
 
-		<tr>
-			<td><label for="tanggal_berkunjung">Tanggal Berkunjung</label></td>
-			<td>:</td>
-			<td><input type="text" name="tanggal_berkunjung" value="<?= $data['tanggal_berkunjung']; ?>"> </td>
-		</tr>	
+<section>
+	<header class="transparan">
+		<div class="container">
+			<nav class="navbar navbar-expand-lg navbar-dark mt-auto mb-2">
+			  		<img src="foto/2Logo-Botani-1.png" height="90" width="90">
+			  	<div class="nav-item">
+				  	<h1 class="text-white text-uppercase font-weight-bolder ml-4 shadow-sm">Bukti Pemesanan Tiket Taman Botani</h1>
+			  	</div>
+			</nav>
+		</div>
+	</header>
+</section>
 
-		<tr>
-			<td><label for="no_telp">No. Telepon</label></td>
-			<td>:</td>
-			<td><input type="text" name="no_telp" value="<?= $data['no_telp']; ?>"> </td>
-		</tr>
+<!-- Akhir Header -->
 
-		<tr>
-			<td><label for="jumlah_tiket">Jumlah tiket</label></td>
-			<td>:</td>
-			<td><input type="text" name="jumlah_tiket" required value="<?= $data['jumlah_tiket']; ?>"> </td>
-		</tr>
-		
-		<tr>
-			<td><label for="total_pembayaran">Total pembayaran</label></td>
-			<td>:</td>
-			<td><input type="text" name="total_pembayaran" value="<?= $data['total_pembayaran']; ?>"> </td>
-		</tr>
-	</table>
-		<button name="printdetail" type="submit" class="btn btn-primary"><i class="fas fa-print"></i></button>
-	</form>
-	</div>
-	</div>
-	<br><br>
-	<?php endforeach; ?>
+<!-- Konten -->
+
+<section>	
+	<content>
+		<div class="container">    
+			<?php foreach ($printpemesanan as $data): ?>
+			<div class="container">
+			<div class="content">
+			<form method="POST" action="">
+			<table>
+				<tr>
+					<td><label for="nama_pemesan">Nama Pemesan</label></td>
+					<td>:</td>
+					<td><Input type="text" name="nama_pemesan" value="<?= $data["nama_pemesan"]; ?>"> </td>
+				</tr>	
+
+				<tr>
+					<td><label for="alamat">Alamat</label></td>
+					<td>:</td>
+					<td><input type="text" name="alamat" value="<?= $data['alamat']; ?>"> </td>
+				</tr>
+
+				<tr>
+					<td><label for="tanggal_berkunjung">Tanggal Berkunjung</label></td>
+					<td>:</td>
+					<td><input type="text" name="tanggal_berkunjung" value="<?= $data['tanggal_berkunjung']; ?>"> </td>
+				</tr>	
+
+				<tr>
+					<td><label for="no_telp">No. Telepon</label></td>
+					<td>:</td>
+					<td><input type="text" name="no_telp" value="<?= $data['no_telp']; ?>"> </td>
+				</tr>
+
+				<tr>
+					<td><label for="jumlah_tiket">Jumlah tiket</label></td>
+					<td>:</td>
+					<td><input type="text" name="jumlah_tiket" required value="<?= $data['jumlah_tiket']; ?>"> </td>
+				</tr>
+				
+				<tr>
+					<td><label for="total_pembayaran">Total pembayaran</label></td>
+					<td>:</td>
+					<td><input type="text" name="total_pembayaran" value="<?= $data['total_pembayaran']; ?>"> </td>
+				</tr>
+			</table>
+				<button name="printdetail" type="submit" class="btn btn-primary"><i class="fas fa-print"></i></button>
+			</form>
+			</div>
+			</div>
+			<br><br>
+			<?php endforeach; ?>
+		</div>
+	</content>
+</section>
+
+<!-- Akhir Konten -->
+
 </body>
 </html>
